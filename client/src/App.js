90421ce73e6cf245c68c312821e0b903b06d30c6
Nnamdi Ibe <nnam.ibe@gmail.com>
2019-11-23 10:10:50 -0500

    Updated html

--- client/src/App.js
+++ client/src/App.js
@@ -6,18 +6,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Interestify</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React Frontend
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
